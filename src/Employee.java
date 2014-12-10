de7ab9f9a0a68c1d920a2370162bf3fbb7f06f6d
mama-egg <Kinseygoesrawr@gmail.com>
2014-12-10 08:15:48 -0700

    Update Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -1,1 +1,48 @@
-// class to represent an emplayee - inherits Person
+public class Employee extends Person {
+  
+  super(first, last, age);
+  private double hourlyWage;
+  private int hoursWorked;
+  private String jobTitle;
+  //Add new field manager of type Manager
+  
+  private Employee(String first, String last, int age, double hourlyWage, int hoursWorked, String jobTitle);
+  //Write a constructor that takes 6 parameters - the ones from Peron and the three others.
+  
+  private double getWage() { //
+    return hourlyWage;
+  }
+  
+  private int getHours() { //
+    return hoursWorked;
+  }
+  
+  private double getWeeklySalary() {
+    return hourlyWage*hoursWorked);
+  }
+  
+  private double getYearlySalary() {
+    return (getWeeklySalary())*52;
+  }
+  
+  private String getJobTitle() {
+    return jobTitle;
+  }
+  
+  private String toString() {
+    this.toString();
+    return (Person.toString() + ": " + jobTitle + " @ $" + Employee.getYearlySalary());
+  }
+  
+  private String getManager() {
+    return //return the string value of manager?????
+  }
+  
+  //Mutators
+  private double setWage(double newWage) {
+    hourlyWage = newWage;
+    return hourlyWage;
+  }
+  
+  private double 
+}
