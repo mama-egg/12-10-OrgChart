3e3639445b5ca1aab8576ee3bfbd9e9c4e65f5ef
mama-egg <Kinseygoesrawr@gmail.com>
2014-12-10 21:36:15 -0700

    Update OrgChart.java

--- src/OrgChart.java
+++ src/OrgChart.java
@@ -1,1 +1,25 @@
-// main program to drive org chart
+public class OrgChart extends Manager{
+  
+  private Manager m1() {
+    m1.toString();
+  }
+  private Manager m2() {
+    m2.toString;
+  }
+  
+  private Employee() {
+    Employee.setManager(m1);
+    Employee.toString();
+  }
+  
+  private Employee() {
+    Employee.setManager(m1);
+    Employee.toString();
+  }
+  
+  private Employee() {
+    Employee.setManager(m2);
+    Employee.setManager(m1);
+    Employee.toString();
+  }
+}
