144107e8fd03ca0bff3b3203babe0bf7ee3fcc7d
mama-egg <Kinseygoesrawr@gmail.com>
2014-12-10 21:17:40 -0700

    Update Manager.java

--- src/Manager.java
+++ src/Manager.java
@@ -1,3 +1,34 @@
+import java.util.ArrayList;
+
 public class Manager extends Employee {
   
+  super(first, last, age, hourlyWage, hoursWorked, jobTitle);
+  private ArrayList<String> directReports = new ArrayList<String>();
+  private String department;
+  
+  //Accessors
+  private String getDepartment() {
+    return department;
+  }
+  
+  private String getDirectReports() {
+    return directReports;
+  }
+  
+  private String toString() {
+    return Manager.toString();
+  }
+  
+  //Mutators
+  private void addDirectReport(Employee employee) {
+    directReports.add(employee);
+  }
+  
+  private void removeDirectReport(Employee employee) {
+    directreports.remove(employee);
+  }
+  
+  private String setDepartment(String dept) {
+    department = dept;
+  }
 }
